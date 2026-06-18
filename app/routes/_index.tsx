b303455c3641d1a8e77f8d0bf8aa3ccb38764a61
Mustpha Kura Mohammed <75446049+mmustaphak@users.noreply.github.com>
2026-06-18 14:44:29 +0100

    feat(layout): finished the header and footer for the root layout

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,8 @@
+import email from "../images/email.svg";
+import website from "../images/website.svg";
+import logo from "../images/logo.png";
 import { HiOutlineUserCircle } from "react-icons/hi";
-import { Link, NavLink, Outlet, redirect, useNavigate } from "react-router";
+import { Link, NavLink, Outlet, redirect } from "react-router";
 import { supabase } from "~/supabase";
 
 export async function clientLoader() {
@@ -10,25 +13,65 @@ export async function clientLoader() {
   }
 }
 export default function Home() {
-  let navigate = useNavigate();
+  const navLink = [
+    { label: "Arena", to: "." },
+    { label: "Bookings", to: "bookings" },
+    { label: "History", to: "history" },
+    { label: "Help", to: "help" },
+  ];
+  const renderedNavLinks = navLink.map(({ label, to }) => (
+    <NavLink
+      to={"/" + to}
+      className={({ isActive }) =>
+        isActive ? "border-b-2 border-b-green py-3 font-semibold" : "text-muted-foreground!"
+      }
+    >
+      {label}
+    </NavLink>
+  ));
   return (
     <>
-      <header>
-        <Link to={"/"}>Pitch Velocity</Link>
-        <nav>
-          <NavLink to="/">Arenas</NavLink>
-          <NavLink to="/">Bookings</NavLink>
-          <NavLink to="/">History</NavLink>
-          <NavLink to="/">Help</NavLink>
-        </nav>
-        <HiOutlineUserCircle />
+      <header className="flex justify-between items-center px-6">
+        <Link to={"/"}>
+          <img src={logo} />
+        </Link>
+        <nav className="flex items-center gap-x-6 text-muted-foreground">{renderedNavLinks}</nav>
+        <HiOutlineUserCircle className="size-8 text-green" />
       </header>
+
       <Outlet />
-      <footer>
-        {/* Logo goes here  */}
+
+      <footer className="text-muted-foreground font-medium">
+        <div className="flex container flex-wrap gap-x-40 w-full items-center px-6 py-12">
+          <div>
+            <Link to={"/"}>
+              <img src={logo} />
+            </Link>
+            <p className="mt-4 max-w-100">
+              Elevating the game through professional venue discovery and seamless digital bookings.
+            </p>
+            <div className="flex gap-x-2 mt-2">
+              <Link to="/">
+                <img src={website} alt="" />
+              </Link>
+              <a href="mailto:customersupport@velocitypitch.com">
+                <img src={email} alt="" />
+              </a>
+            </div>
+          </div>
+          <div>
+            <p className="text-[#191C1E] font-semibold">Support</p>
+            <div className="grid gap-y-2 mt-4 text-sm">
+              <p>Support Center</p>
+              <p>Booking Policy</p>
+              <p>Terms of Service</p>
+              <p>Privacy Policy</p>
+            </div>
+          </div>
+        </div>
         <hr />
-        <div>
-          <p>© 2024 Pitch Velocity. All rights reserved.</p>
+        <div className="container px-6 py-12 flex justify-between">
+          <p>© {new Date().getFullYear()}Pitch Velocity. All rights reserved.</p>
           <p>English (UK) GBP (£)</p>
         </div>
       </footer>
